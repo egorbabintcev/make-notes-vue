8c0d6ca0e3c9a0032b31665bb3ffb522a790afcc
Egor Babintsev <e.e.babintsev@yandex.ru>
2021-04-01 14:14:41 +0300

    Create component for note input. Add dynamic textarea resize. Add styles for grid container.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
+<template>
+  <div class="container">
+    <note-input :addNote="() => {}"></note-input>
+  </div>
+</template>
+
 <script>
+import NoteInput from '@/components/NoteInput.vue';
+
 export default {
   name: 'App',
+  components: {
+    NoteInput,
+  },
 };
 </script>
+
+<style lang="scss">
+@import '~@/assets/sass/utils/functions';
+
+.container {
+  max-width: 480px;
+  margin: 0 auto;
+  padding: {
+    left: rem(15);
+    right: rem(15);
+    top: rem(60);
+  }
+
+  // grid
+  display: grid;
+  grid-gap: rem(40) 0;
+  grid-template-rows: repeat(1, auto);
+}
+</style>
---
+++ src/components/NoteInput.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="note-input">
+    <form @submit.prevent="handleSubmit">
+      <textarea
+        placeholder="Title"
+        rows="1"
+        v-model.trim="title"
+        @input="resizeHeight"
+      >
+      </textarea>
+      <textarea
+        placeholder="Description"
+        rows="1"
+        v-model.trim="descr"
+        @input="resizeHeight"
+      >
+      </textarea>
+      <button type="submit">Create <!--<i class="icon">✅</i> --></button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoteInput',
+  props: {
+    addNote: Function,
+  },
+  data() {
+    return {
+      title: '',
+      descr: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const { title, descr } = this;
+      if (title.length > 0 && descr.length > 0) {
+        this.addNote(title, descr);
+        this.title = '';
+        this.descr = '';
+      }
+    },
+    resizeHeight(e) {
+      e.target.style.height = 'auto';
+      e.target.style.height = `${e.target.scrollHeight + 2}px`;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~@/assets/sass/utils/functions";
+
+.note-input {
+  form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+
+    textarea, button {
+      border-radius: rem(4);
+      width: 100%;
+    }
+
+    textarea {
+      border: rem(1) solid hsl(0, 0%, 80%);
+      font-size: rem(22);
+      margin-bottom: rem(10);
+      padding: rem(10);
+      resize: none;
+    }
+
+    button {
+      background-color: hsl(135, 60%, 50%);
+      color: hsl(0, 0%, 100%);
+      padding: rem(10) rem(15);
+      font: {
+        family: 'Inconsolata', monospace;
+        size: rem(20);
+        weight: 700;
+      }
+
+      .icon {
+        font-size: rem(14);
+      }
+    }
+  }
+}
+</style>
